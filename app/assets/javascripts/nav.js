aad7159ac8c991f2de8c6cde6d5adead468f74ac
Tigran Terian <tigran.terian@gmail.com>
2015-03-23 14:43:29 +0400

    Fixed main navigation

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
 	//open-close submenu on mobile
 	$('#cd-menu').on('click', function(event){
-		if($(event.target).is('#cd-menu')) $('.cd-main-nav').children('ul').toggleClass('is-visible');
+		if($(event.target).is('#cd-menu')) $('.cd-main-nav').toggleClass('is-visible');
 	});
 });
